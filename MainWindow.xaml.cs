cad7ebe24808ae8968d3d90085b4fe6b7f4435f0
Prokke <rybin.pave@yandex.ru>
2024-03-10 21:27:14 +0500

    window template + Initialize Chromium

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +18,53 @@ using System.Windows.Shapes;
 
 namespace AI_Client
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private ChromiumWebBrowser chromeBrowser;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeChromium();
+        }
+
+        private void InitializeChromium()
+        {
+            try
+            {
+                CefSettings settings = new CefSettings
+                {
+                    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 /CefSharp Browser" + Cef.CefSharpVersion,
+                    CachePath = Directory.GetCurrentDirectory() + "\\cache"
+
+                };
+
+
+                Cef.Initialize(settings);
+
+                chromeBrowser = new ChromiumWebBrowser();
+                browsergrid.Children.Add(chromeBrowser); 
+                Canvas.SetLeft(chromeBrowser, 0);
+                Canvas.SetTop(chromeBrowser, 0);
+                chromeBrowser.Address = "about:blank"; 
+                chromeBrowser.FrameLoadEnd += ChromeBrowser_FrameLoadEnd;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка инициализации Chromium: {ex.Message}");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ChromeBrowser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
+        {
+            Dispatcher.Invoke(() => ProggresBar.Visibility = Visibility.Hidden);
+        }
+
+        private void ReloadButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Models/ProxySettings.cs
@@ -0,0 +1,14 @@
+﻿namespace AI_Client.Models
+{
+    public class ProxySettings
+    {
+
+        public string ProxyName { get; set; }
+        public string ProxyIP { get; set; }
+        public string ProxyPort { get; set; }
+        public string ProxyUsername { get; set; }
+        public string ProxyPassword { get; set; }
+        public bool LastUsingProxy { get; set; }
+
+    }
+}
